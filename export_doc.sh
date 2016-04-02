0a0cdc814de9ea3c810f4000c83a5c58b73c02dc
Nerijus Arlauskas <nercury@gmail.com>
2016-04-02 16:34:29 +0300

    Use features when generating doc.

--- export_doc.sh
+++ export_doc.sh
@@ -11,7 +11,7 @@ fi
 echo "exporting docs"
 
 export PATH=$HOME/.local/bin:$PATH
-cargo doc
+cargo doc --features "$FEATURES"
 echo "<meta http-equiv=refresh content=0;url=${CRATE}/index.html>" > target/doc/index.html
 pip install ghp-import --user `whoami`
 ghp-import -n target/doc
